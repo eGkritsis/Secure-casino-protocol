c0c2a091f7a46001fc4b02ba7f64ee7612666945
ilias37 <iliaspanag37@outlook.com>
2025-03-12 23:03:52 +0200

    Refactored SecurityConfig

--- src/main/java/com/aueb/casino/netsec/demo/config/SecurityConfig.java
+++ src/main/java/com/aueb/casino/netsec/demo/config/SecurityConfig.java
@@ -1,24 +1,18 @@
 package com.aueb.casino.netsec.demo.config;
 
-import com.aueb.casino.netsec.demo.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    @Autowired
-    CustomUserDetailsService customUserDetailsService;
-
-    @Autowired
-    PasswordEncoderConfig passwordEncoderConfig;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -43,9 +37,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoderConfig.passwordEncoder());
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+
 }
