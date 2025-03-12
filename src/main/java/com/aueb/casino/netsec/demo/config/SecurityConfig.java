c4979d4e670a5536dc3d8d0a639a4897812b8aee
eGkritsis <evgeniosgkritsis@gmail.com>
2025-03-12 16:21:15 +0200

    Implement SecurityConfig

--- src/main/java/com/aueb/casino/netsec/demo/config/SecurityConfig.java
+++ src/main/java/com/aueb/casino/netsec/demo/config/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.aueb.casino.netsec.demo.config;
 
+import com.aueb.casino.netsec.demo.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    PasswordEncoderConfig passwordEncoderConfig;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,8 +42,9 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoderConfig.passwordEncoder());
+
     }
 }
